7517d07c6307029ec7e212c143a81bf094a076e0
Devan <devandbenz@gmail.com>
2024-07-23 21:51:51 -0500

    feat(wip): refactor
    Signed-off-by: Devan <devandbenz@gmail.com>

--- src/merger/file_manager.rs
+++ src/merger/file_manager.rs
@@ -1,3 +1,8 @@
+use std::ffi::OsStr;
+use std::fs::File;
+use std::path::Path;
+use std::sync::Arc;
+
 use arrow::array::{
     ArrayRef, BinaryArray, BooleanArray, Date32Array, Date64Array, Decimal128Array,
     Decimal256Array, DurationNanosecondArray, FixedSizeBinaryArray, Float16Array, Float32Array,
@@ -11,10 +16,6 @@ use parquet::arrow::arrow_reader::ParquetRecordBatchReaderBuilder;
 use parquet::arrow::ArrowWriter;
 use parquet::basic::Compression;
 use parquet::file::properties::WriterProperties;
-use std::fs::File;
-use std::path::Path;
-use std::sync::Arc;
-use clap::builder::OsStr;
 
 pub struct FileManager {
     file_paths: Vec<String>,
@@ -23,7 +24,7 @@ pub struct FileManager {
 }
 
 impl FileManager {
-    pub fn new(file_paths: Vec<String>, schema: SchemaRef, output_file_path: String) -> Self {
+    pub async fn new(file_paths: Vec<String>, schema: SchemaRef, output_file_path: String) -> Self {
         Self {
             file_paths,
             schema,
@@ -31,7 +32,14 @@ impl FileManager {
         }
     }
 
-    pub fn read_to_arrow_batches(&self) {
+    pub async fn merge_files(&self) {
+        let combined_file = File::create_new(Path::new(&self.output_file_path)).unwrap();
+        let arrow_schema = &self.schema;
+        let owned_arrow_schema = arrow_schema.to_owned();
+        let file_paths = &self.file_paths;
+    }
+
+    pub async fn write_record_batch_to_parquet(&self) {
         let combined_file = File::create_new(Path::new(&self.output_file_path)).unwrap();
         let arrow_schema = &self.schema;
         let owned_arrow_schema = arrow_schema.to_owned();
@@ -67,123 +75,7 @@ impl FileManager {
                     let col = record_batch.column_by_name(a.name()).unwrap();
                     col.to_owned()
                 }
-                Err(_) => {
-                    // Add default values if the field is missing
-                    match field.data_type() {
-                        DataType::Int64 => {
-                            Arc::new(Int64Array::from(vec![None; record_batch.num_rows()]))
-                                as ArrayRef
-                        }
-                        DataType::Date32 => {
-                            Arc::new(Date32Array::from(vec![None; record_batch.num_rows()]))
-                                as ArrayRef
-                        }
-                        DataType::Null => {
-                            Arc::new(NullArray::new(record_batch.num_rows())) as ArrayRef
-                        }
-                        DataType::Boolean => {
-                            Arc::new(BooleanArray::from(vec![None; record_batch.num_rows()]))
-                                as ArrayRef
-                        }
-                        DataType::Int8 => {
-                            Arc::new(Int8Array::from(vec![None; record_batch.num_rows()]))
-                                as ArrayRef
-                        }
-                        DataType::Int16 => {
-                            Arc::new(Int16Array::from(vec![None; record_batch.num_rows()]))
-                                as ArrayRef
-                        }
-                        DataType::Int32 => {
-                            Arc::new(Int32Array::from(vec![None; record_batch.num_rows()]))
-                                as ArrayRef
-                        }
-                        DataType::UInt8 => {
-                            Arc::new(UInt8Array::from(vec![None; record_batch.num_rows()]))
-                                as ArrayRef
-                        }
-                        DataType::UInt16 => {
-                            Arc::new(UInt16Array::from(vec![None; record_batch.num_rows()]))
-                                as ArrayRef
-                        }
-                        DataType::UInt32 => {
-                            Arc::new(UInt32Array::from(vec![None; record_batch.num_rows()]))
-                                as ArrayRef
-                        }
-                        DataType::UInt64 => {
-                            Arc::new(UInt64Array::from(vec![None; record_batch.num_rows()]))
-                                as ArrayRef
-                        }
-                        DataType::Float16 => {
-                            Arc::new(Float16Array::from(vec![None; record_batch.num_rows()]))
-                                as ArrayRef
-                        }
-                        DataType::Float32 => {
-                            Arc::new(Float32Array::from(vec![None; record_batch.num_rows()]))
-                                as ArrayRef
-                        }
-                        DataType::Float64 => {
-                            Arc::new(Float64Array::from(vec![None; record_batch.num_rows()]))
-                                as ArrayRef
-                        }
-                        DataType::Timestamp(_, _) => Arc::new(TimestampNanosecondArray::from(vec![
-                                None;
-                                record_batch.num_rows()
-                            ])) as ArrayRef, // Assuming nanoseconds precision
-                        DataType::Date64 => {
-                            Arc::new(Date64Array::from(vec![None; record_batch.num_rows()]))
-                                as ArrayRef
-                        }
-                        DataType::Time32(_) => {
-                            Arc::new(Time32SecondArray::from(vec![None; record_batch.num_rows()]))
-                                as ArrayRef
-                        } // Assuming seconds
-                        DataType::Time64(_) => Arc::new(Time64NanosecondArray::from(vec![
-                                None;
-                                record_batch.num_rows()
-                            ])) as ArrayRef, // Assuming nanoseconds
-                        DataType::Duration(_) => Arc::new(DurationNanosecondArray::from(vec![
-                                None;
-                                record_batch.num_rows()
-                            ])) as ArrayRef, // Assuming nanoseconds
-                        DataType::Interval(_) => Arc::new(IntervalYearMonthArray::from(vec![
-                                None;
-                                record_batch.num_rows()
-                            ])) as ArrayRef, // Assuming year-month interval
-                        DataType::Binary => {
-                            Arc::new(BinaryArray::from(vec![None; record_batch.num_rows()]))
-                                as ArrayRef
-                        }
-                        DataType::FixedSizeBinary(_) => Arc::new(FixedSizeBinaryArray::from(vec![
-                                None;
-                                record_batch.num_rows()
-                            ])) as ArrayRef,
-                        DataType::LargeBinary => {
-                            Arc::new(LargeBinaryArray::from(vec![None; record_batch.num_rows()]))
-                                as ArrayRef
-                        }
-                        DataType::Utf8 => Arc::new(StringArray::from(vec![
-                            String::from("");
-                            record_batch.num_rows()
-                        ])) as ArrayRef,
-                        // DataType::LargeUtf8 => Arc::new(LargeStringArray::from(vec![None; record_batch.num_rows()])) as ArrayRef,
-                        // DataType::List(a) => Arc::new(ListArray::from(vec![None; record_batch.num_rows()])) as ArrayRef, // Placeholder, may need custom handling
-                        // DataType::FixedSizeList(_, size) => Arc::new(FixedSizeListArray::from(vec![None; record_batch.num_rows()])) as ArrayRef, // Placeholder
-                        // DataType::LargeList(_) => Arc::new(LargeListArray::from(vec![None; record_batch.num_rows()])) as ArrayRef, // Placeholder
-                        // DataType::Struct(_) => Arc::new(StructArray::from(vec![None; record_batch.num_rows()])) as ArrayRef, // Placeholder, needs custom handling
-                        // DataType::Union(_, _) => Arc::new(UnionArray::from(vec![None; record_batch.num_rows()])) as ArrayRef, // Placeholder, needs custom handling
-                        // DataType::Dictionary(_, _) => Arc::new(DictionaryArray::from(vec![None; record_batch.num_rows()])) as ArrayRef, // Placeholder, needs custom handling
-                        DataType::Decimal128(_, _) => {
-                            Arc::new(Decimal128Array::from(vec![None; record_batch.num_rows()]))
-                                as ArrayRef
-                        }
-                        DataType::Decimal256(_, _) => {
-                            Arc::new(Decimal256Array::from(vec![None; record_batch.num_rows()]))
-                                as ArrayRef
-                        }
-                        // DataType::Map(_, _) => Arc::new(MapArray::from(vec![None; record_batch.num_rows()])) as ArrayRef, // Placeholder, needs custom handling
-                        _ => unimplemented!("Type not supported"),
-                    }
-                }
+                Err(_) => Self::create_default_array(field.data_type(), record_batch.num_rows()),
             };
             columns.push(array);
         }
@@ -196,8 +88,6 @@ impl FileManager {
         schema: &SchemaRef,
         record_batch: RecordBatch,
     ) {
-        println!("writing record batch: {:#?}", record_batch.num_rows());
-
         let props = WriterProperties::builder()
             .set_compression(Compression::SNAPPY)
             .build();
@@ -206,6 +96,58 @@ impl FileManager {
         writer.write(&record_batch).expect("Writing batch");
 
         writer.close().unwrap();
+    }
+
+    fn create_default_array(data_type: &DataType, num_rows: usize) -> ArrayRef {
+        match data_type {
+            DataType::Int64 => Arc::new(Int64Array::from(vec![None; num_rows])) as ArrayRef,
+            DataType::Date32 => Arc::new(Date32Array::from(vec![None; num_rows])) as ArrayRef,
+            DataType::Null => Arc::new(NullArray::new(num_rows)) as ArrayRef,
+            DataType::Boolean => Arc::new(BooleanArray::from(vec![None; num_rows])) as ArrayRef,
+            DataType::Int8 => Arc::new(Int8Array::from(vec![None; num_rows])) as ArrayRef,
+            DataType::Int16 => Arc::new(Int16Array::from(vec![None; num_rows])) as ArrayRef,
+            DataType::Int32 => Arc::new(Int32Array::from(vec![None; num_rows])) as ArrayRef,
+            DataType::UInt8 => Arc::new(UInt8Array::from(vec![None; num_rows])) as ArrayRef,
+            DataType::UInt16 => Arc::new(UInt16Array::from(vec![None; num_rows])) as ArrayRef,
+            DataType::UInt32 => Arc::new(UInt32Array::from(vec![None; num_rows])) as ArrayRef,
+            DataType::UInt64 => Arc::new(UInt64Array::from(vec![None; num_rows])) as ArrayRef,
+            DataType::Float16 => Arc::new(Float16Array::from(vec![None; num_rows])) as ArrayRef,
+            DataType::Float32 => Arc::new(Float32Array::from(vec![None; num_rows])) as ArrayRef,
+            DataType::Float64 => Arc::new(Float64Array::from(vec![None; num_rows])) as ArrayRef,
+            DataType::Timestamp(_, _) => {
+                Arc::new(TimestampNanosecondArray::from(vec![None; num_rows])) as ArrayRef
+            }
+            DataType::Date64 => Arc::new(Date64Array::from(vec![None; num_rows])) as ArrayRef,
+            DataType::Time32(_) => {
+                Arc::new(Time32SecondArray::from(vec![None; num_rows])) as ArrayRef
+            }
+            DataType::Time64(_) => {
+                Arc::new(Time64NanosecondArray::from(vec![None; num_rows])) as ArrayRef
+            }
+            DataType::Duration(_) => {
+                Arc::new(DurationNanosecondArray::from(vec![None; num_rows])) as ArrayRef
+            }
+            DataType::Interval(_) => {
+                Arc::new(IntervalYearMonthArray::from(vec![None; num_rows])) as ArrayRef
+            }
+            DataType::Binary => Arc::new(BinaryArray::from(vec![None; num_rows])) as ArrayRef,
+            DataType::FixedSizeBinary(_) => {
+                Arc::new(FixedSizeBinaryArray::from(vec![None; num_rows])) as ArrayRef
+            }
+            DataType::LargeBinary => {
+                Arc::new(LargeBinaryArray::from(vec![None; num_rows])) as ArrayRef
+            }
+            DataType::Utf8 => {
+                Arc::new(StringArray::from(vec![String::from(""); num_rows])) as ArrayRef
+            }
+            DataType::Decimal128(_, _) => {
+                Arc::new(Decimal128Array::from(vec![None; num_rows])) as ArrayRef
+            }
+            DataType::Decimal256(_, _) => {
+                Arc::new(Decimal256Array::from(vec![None; num_rows])) as ArrayRef
+            }
+            _ => unimplemented!("Type not supported"),
+        }
     }
 }
 
